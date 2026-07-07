c0d218a54597cc6da7302f7bc11f779e23ac84f5
近藤直哉 <waoya97@gmail.com>
2026-07-07 11:30:08 +0900

    npm追加

--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,7 @@
 const config = {
-  apiUrl: 'http://localhost:5955'
+  apiUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:5955'
+    : `http://${window.location.hostname}:5955`
 };
 
 export default config;
